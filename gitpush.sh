e6a1fcfbbb8f790d2a9eb6382d9338cfc0759bdd
naveen272 <naveencheekati2@gmail.com>
2021-10-06 12:35:44 +0530

    new db backup and docs

--- gitpush.sh
+++ gitpush.sh
@@ -3,9 +3,9 @@
 #description    :This script will Script to update all files to git;
 #usage          :./gitpush.sh
 
-git status >> /var/www/html/logs.txt
-git add . >> /var/www/html/logs.txt
-git status >> /var/www/html/logs.txt
-git commit -m "new db backup and docs" >> /var/www/html/logs.txt
-git push origin master >> /var/www/html/logs.txt
+git status >> /home/ubuntu/gitlogs.txt
+git add . >> /home/ubuntu/gitlogs.txt
+git status >> /home/ubuntu/gitlogs.txt
+git commit -m "new db backup and docs" >> /home/ubuntu/gitlogs.txt
+git push origin master >> /home/ubuntu/gitlogs.txt
 
